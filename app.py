9186b742f6847a97f5d447515c0b61fe0625c9c9
Ankit Wankhede <73584138+ankit-rl4@users.noreply.github.com>
2021-10-10 22:30:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,49 @@
-from chatterbot import ChatBot
-from chatbot import chatbot
-from flask import Flask, render_template, request
+class chat_bot:
+    def __init__(self):
+        self.keywords=[]
+        self.text=''
+        self.word=''
+        self.tolist()
+    def get_response(self,User_text):
+        self.text=User_text.lower().strip()
+        for x in self.keywords:
+            if self.isWordPresent(self.text,x):
+                self.word=x
+                break
+        with open('training_data/more.txt','r') as file1:
+            flag=0
+            for line in file1:
+                if line.strip()=='':
+                    pass
+                elif flag==2:
+                    flag=0
+                    return line 
+                elif flag==0:
+                    if line.lower().strip()==self.word:
+                        flag=2
+                elif flag==1:
+                    flag=0
 
-app = Flask(__name__)
-app.static_folder = 'static'
+        return "Sorry I can't understand"
 
-@app.route("/")
-def home():
-    return render_template("index.html")
 
-@app.route("/get")
-def get_bot_response():
-    userText = request.args.get('msg')
-    return str(chatbot.get_response(userText))
+    def tolist(self):
+        with open('training_data/more.txt','r') as file1:
+            flag=0
+            for line in file1:
+                if line.strip()=='':
+                    pass
+                elif flag==0:
+                    flag=1
+                    word=line.strip()
+                    self.keywords.append(word.lower())
+                elif flag==1:
+                    flag=0
+        self.keywords.sort(key =len ,reverse=True)
 
-if __name__ == "__main__":
-    app.run()
+
+    def isWordPresent(self,sentence, word):
+        s = sentence.find(word)
+        if s != -1:
+            return True
+        return False
